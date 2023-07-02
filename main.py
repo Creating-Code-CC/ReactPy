dec17bcb13887a3347090e2763213d7809d54ce5
Malik Smith <smithmalik2468@gmail.com>
2023-07-02 05:07:46 +0000

    My to do list successful

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from starlette.applications import Starlette
 
 @component
 def HelloWorld():
-    return html.h1("Hello, world!")
+    return html.div(
+            html.h1("My Todo List"),
+            html.ul(
+                html.li("Design a cool new app"),
+                html.li("Build it"),
+                html.li("Share it with the world!"),
+            )
+        )
 
 
 app = Starlette()
