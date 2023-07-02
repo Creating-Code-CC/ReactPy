aeb183f9968671679826b2b0fa7bb7f8c136976c
Malik Smith <smithmalik2468@gmail.com>
2023-07-02 05:29:10 +0000

    running first component successful

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from reactpy import component, html
+from reactpy import component, html, run
 from reactpy.backend.starlette import configure
 from starlette.applications import Starlette
 
 @component
-def HelloWorld():
-    return html.div(
-            html.h1("My Todo List"),
-            html.ul(
-                html.li("Design a cool new app"),
-                html.li("Build it"),
-                html.li("Share it with the world!"),
-            )
-        )
+def Photo():
+    return html.img(
+        {
+            "src": "https://picsum.photos/id/237/500/300",
+            "style": {"width": "50%"},
+            "alt": "Puppy",
+        }
+    )
+
+
+run(Photo)
 
 
 app = Starlette()
